800127f2124907020dea015b47951ad9667ade3e
fadedheartlife <fadedheartlife@gmail.com>
2026-03-23 18:06:59 -1200

    Create User.js model file with password hashing and comparison methods

---
+++ models/User.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+// Hash the password before saving
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password') || this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+// Compare password method
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
